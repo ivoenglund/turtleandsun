4df475af3478df3c657d878b805f26c6d654d2e7
ivoenglund <ivo.englund@gmail.com>
2026-06-05 19:07:16 +0200

    Fix: add yt_video_id to clips query, add calendar HTML to body, remove delete button

--- server.js
+++ server.js
@@ -6877,6 +6877,7 @@ app.get('/admin/api/tracker/clips', requireRole('admin'), async (req, res) => {
         sc.style, sc.mood, sc.notes,
         COALESCE(sc.custom_tags, '{}') AS custom_tags,
         sc.output_url,
+        sc.yt_video_id,
         sc.created_at,
         (sc.tiktok_posted_at IS NOT NULL OR sc.published_tiktok)      AS tiktok_posted,
         (sc.instagram_posted_at IS NOT NULL OR sc.published_instagram) AS instagram_posted,
