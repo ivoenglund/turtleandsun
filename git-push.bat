4b1a10685f84532a3e1369b3cb179889c73716c2
ivoenglund <ivo.englund@gmail.com>
2026-06-09 04:27:50 +0200

    feat: copy buttons for caption/hashtags + download video in TikTok modal

--- git-push.bat
+++ git-push.bat
@@ -2,6 +2,6 @@
 cd /d C:\turtleandsun
 if exist .git\index.lock del /q /f .git\index.lock
 git add -A
-git commit -m "fix: update TikTok extension URL to tiktokstudio/upload"
+git commit -m "feat: copy buttons for caption/hashtags + download video in TikTok modal"
 git push origin main
 pause
